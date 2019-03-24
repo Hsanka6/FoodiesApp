389fff0251c99e96e8ef8e9a18c6416cd3a2e6d4
Hsanka6 <haasith2@gmail.com>
2019-03-24 11:55:47 -0700

    Initial Commit

---
+++ FoodiesTests/FoodiesTests.swift
@@ -0,0 +1,34 @@
+//
+//  FoodiesTests.swift
+//  FoodiesTests
+//
+//  Created by Haasith Sanka on 3/24/19.
+//  Copyright © 2019 Haasith Sanka. All rights reserved.
+//
+
+import XCTest
+@testable import Foodies
+
+class FoodiesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
